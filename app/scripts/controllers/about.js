f0a376c41b41bf7b8fe44650d43eeab862166844
nahuel.c <nahuel.c@geopagos.com>
2016-10-22 14:08:48 -0300

    - First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name itcrowdchallengeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the itcrowdchallengeApp
+ */
+angular.module('itcrowdchallengeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name itcrowdchallengeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the itcrowdchallengeApp
+ */
+angular.module('itcrowdchallengeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
