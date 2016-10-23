f927b04a3f6d67ceb9bc6b2d488b4b4d0aff7e03
ncarreiro <nahuelcarreiro@gmail.com>
2016-10-23 00:09:15 -0300

    - Added: Dragula $scope.$on for features testing that were removed by mistake.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,4 +20,24 @@ angular.module('itcrowdchallengeApp')
     $scope.updateList = function () {
         ServerInterface.getList();
     };
+
+    $scope.$on('task-item.drag', function (e, el) {
+        console.log(el);
+        el.removeClass('moved');
+    });
+
+    $scope.$on('task-item.drop', function (e, el) {
+        console.log(el);
+        el.addClass('moved');
+    });
+
+    $scope.$on('task-item.over', function (e, el, container) {
+        console.log(container);
+        container.addClass('over');
+    });
+
+    $scope.$on('task-item.out', function (e, el, container) {
+        console.log(container);
+        container.removeClass('over');
+    });
   });
